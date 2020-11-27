31538c7fa138e5c51e01403149a45003dcaacf7e
Gustavo Minari <gustavo_minari@hotmail.com.br>
2020-11-26 21:20:42 -0300

    update electron

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow } = require("electron");
 
 function createWindow() {
-    let winLoad = new BrowserWindow({
-		frame: true,
-		icon: 'qr-code.ico',
-		title: "Criado QRCode",
-        simpleFullscreen: true,
-        show: false,
-    });
-    winLoad.setMenuBarVisibility(false);
-	winLoad.on('ready-to-show', () => { if(winLoad != null) { winLoad.show(); winLoad.focus(); winLoad.maximize(); } });
-	winLoad.on('close', () => { winLoad = null; });
-	winLoad.loadURL(`file://${__dirname}/view/index.html`);
-	return winLoad;
+  let winLoad = new BrowserWindow({
+    frame: true,
+    icon: "qr-code.ico",
+    title: "QRCode Created",
+    simpleFullscreen: true,
+    show: false,
+  });
+  winLoad.setMenuBarVisibility(false);
+  winLoad.on("ready-to-show", () => {
+    if (winLoad != null) {
+      winLoad.show();
+      winLoad.focus();
+      winLoad.maximize();
+    }
+  });
+  winLoad.on("close", () => {
+    winLoad = null;
+  });
+  winLoad.loadURL(`file://${__dirname}/view/index.html`);
+  return winLoad;
 }
 
-app.on('ready', createWindow);
+app.on("ready", createWindow);
